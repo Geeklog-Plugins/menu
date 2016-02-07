81a87f018d331ed9ab6696ba82f4c40e41c884e0
hostellerie <hostellerie.org@gmail.com>
2016-02-07 10:27:51 +0100

    Remove LB on pi_version

--- autoinstall.php
+++ autoinstall.php
@@ -51,8 +51,7 @@ function plugin_autoinstall_menu($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '1.2.6
-		',
+        'pi_version'      => '1.2.6',
         'pi_gl_version'   => '1.8.0',
         'pi_homepage'     => 'http://geeklog.fr'
     );
